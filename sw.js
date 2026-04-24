56e59035d091cdb1248a526e7f8a782f917b9fe2
khaq22 <63936847+khaq22@users.noreply.github.com>
2026-04-25 06:09:36 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'smarttour-cache-v1';
+const CACHE_NAME = 'smarttour-cache-v2';
 const urlsToCache = [
   '/PrototypeSmartTourism_App/',
   '/PrototypeSmartTourism_App/index.html'
